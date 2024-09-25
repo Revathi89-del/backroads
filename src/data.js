f237cb9418dc7f8575366ca4c2798d165c708752
Revathi Vijayaragavan <revathi.serene31@gmail.com>
2024-09-25 11:12:07 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+
+
+
+
+
+
+export const pageLinks = [{id:1 ,href:'#home',text:'home'},
+{id:2,href:'#about',text:'about'},
+{id:3,href:'#services',text:'service'},
+{id:4,href:'#tour',text:'tour'}
+]
+
+export const socialLinks =[
+    {
+        id:1,href:'https://www.facebook.com',icon:'fab fa-facebook'},
+       { id:2,href:'https://www.twitter.com',icon:'fab fa-twitter'},
+        {id:3,href:'https://www.twitter.com',icon:'fab fa-squarespace'}
+    ]
+
+    export const services =[
+    {id:1,icon:"fas fa-wallet fa-fw",title:"saving money",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, official"},
+    {id:2,icon:"fas fa-tree fa-fw",title:"endless hiking",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, official"},
+    {id:3,icon:"fas fa-socks fa-fw",title:"amazing comfort",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, official"}
+    ]
+export const tours =[
+    {id:1,image:tour1,date:"october 1th, 2020",title:"best of Java",duration:6,cost:"2100",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"Indonesia"},
+    {id:2,image:tour2,date:"December 21th, 2020",title:"Tibet adventure",duration:8,cost:"1500",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"Indonesia"},
+    {id:3,image:tour3,date:"May 11th, 2020",title:"best of Java",duration:4,cost:"3200",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"Indonesia"},
+    {id:4,image:tour4,date:"April 13h, 2020",title:"Indonesia lights",duration:7,cost:"2600",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"Indonesia"},
+    {id:4,image:tour5,date:"June 13h, 2020",title:"Malaysia days",duration:7,cost:"3600",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"Malaysia"},
+    {id:4,image:tour6,date:"January 1, 2020",title:"India Delight",duration:7,cost:"1800",info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",location:"India"}
+]
